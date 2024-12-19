a60bae241a692a3f29e825b12c2607f83ed02b52
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2024-12-19 23:14:29 +0300

    Update imports.py

--- imports.py
+++ imports.py
@@ -1,21 +1,19 @@
-# imports.py
-
 import streamlit as st
 import pandas as pd
 import google.generativeai as genai
 
 # Access the API key from Streamlit secrets
-API_KEY = st.secrets["GeminiKey"]
+API_KEY = st.secrets["GeminiKey"] 
 
 # Configure Genai Key
 genai.configure(api_key=API_KEY)
 
 # Gemini model settings
-MODEL_NAME = "gemini-pro"
+MODEL_NAME = "gemini-pro" 
 
-# Load the DataFrame
-df = pd.read_csv("healthcare_data.csv")
+# Load the DataFrame 
+df = pd.read_csv("healthcare_data.csv") 
 
 # Import from the correct location
 from model import get_gemini_response
-from utils.database import get_data_from_df 
+from utils.database import get_data_from_df
